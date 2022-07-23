f9f839d9c9395227f441ff421464edb4d53363f1
root <root@admin.lab.local>
2022-07-23 12:38:56 +0200

    fix

--- oar3/build-images/oar/start-almighty.sh
+++ oar3/build-images/oar/start-almighty.sh
@@ -2,6 +2,14 @@
 
 /bin/bash /generate-config.sh
 
+sleep 5
+
 oar-database --create --db-admin-user root --db-admin-pass azerty --db-ro-user oar_ro --db-ro-pass azerty --conf /etc/oar/oar.conf
+
+sleep 2
+
+/usr/local/sbin/oarproperty -a cpu -c -a host
+
+sleep 2
 
 /usr/local/sbin/almighty
