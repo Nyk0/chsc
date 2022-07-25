6f8080d45df8d0b56d76e7b6bd02d4d361c445fc
root <root@admin.lab.local>
2022-07-25 18:19:32 +0200

    fix

--- oar/build-images/oar/start-node.sh
+++ oar/build-images/oar/start-node.sh
@@ -4,4 +4,18 @@
 
 sleep 2
 
-/usr/sbin/sshd -D -f /etc/oar/sshd_config
+/usr/sbin/sshd -f /etc/oar/sshd_config
+
+until host `hostname`
+do
+	echo "Attempt to resolve ..."
+	sleep 2
+done
+
+echo "Register"
+oarnodesetting -h `hostname` -s Alive
+
+while true
+do
+	sleep 30
+done
